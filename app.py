a8dfdab66a25175911d93ddf3ecdf0e29dbb4e31
ruturaj <ruturajgidde6460@gmail.com>
2025-10-03 14:37:56 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     hostname = os.environ.get("HOSTNAME", "localhost")  # default to "localhost"
-    return f"<h1>Welcome to Ruturaj's Flask App! {hostname}</h1><p>Visit /name or /phone to see details.</p>"
+    return f"<h1>Welcome to Ruturaj  Flask App! {hostname}</h1><p>Visit /name or /phone to see details.</p>"
 
 @app.route("/name")
 def lwname():
